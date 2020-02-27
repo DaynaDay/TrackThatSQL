a60c596830a6b739bad4a69a287f00b504741cb6
Dayna Knight <dknighted11@gmail.com>
2020-02-27 14:00:02 -0500

    added more to other tables. began working in workbench. Installed dependencies.

--- EmployeeTracker.sql
+++ EmployeeTracker.sql
@@ -6,21 +6,43 @@ USE workerBee_db;
 
 CREATE TABLE department (
  id INT PRIMARY KEY,
- nameFull VARCHAR(30)
+ name VARCHAR(30)
 );
 
-INSERT INTO department (nameFull)
-VALUES ();
+INSERT INTO department (id,name)
+VALUES ("Cosmetics");
 
+INSERT INTO department (id, name)
+VALUES ("Building Services");
 
+INSERT INTO department (id, name)
+VALUES ("Young Contemporary");
+
+INSERT INTO department (id, name)
+VALUES ("Childrens");
+-- //////////////////////////////////////////////
 
 CREATE TABLE roles (
 id INT PRIMARY KEY,
 title VARCHAR(30),
 salary DECIMAL,
 department_id INT 
-
 );
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Make Up Artist", 340000,"Cosmetics" );
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Personal Stylist", 340000,"Young Contemporary" );
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Con Artist", 340000,"Childrens" );
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Cable Guy", 340000,"Building Services" );
+
+
+-- //////////////////////////////////////////
 
 CREATE TABLE employee (
 
@@ -31,6 +53,16 @@ CREATE TABLE employee (
   manager_id INT NULL
 );
 
+INSERT INTO department (first_name, last_name, role_id, manager_id)
+VALUES ("Dean", "Winchester", );
 
+INSERT INTO department (nameFull)
+VALUES ("Klaus", "Hargrove");
+
+INSERT INTO department (nameFull)
+VALUES ("Mary Sue", "Trope");
+
+INSERT INTO department (nameFull)
+VALUES ("Larry", "the Cable Guy");
 
   
